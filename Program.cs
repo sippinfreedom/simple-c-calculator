93fc8eaa2b64d550d68e7c910adf32b2018fbf07
Freedom <58274474+sippinfreedom@users.noreply.github.com>
2020-09-13 17:27:45 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter a Number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.Write(num1 + num2);
+            } else if (op == "-")
+            {
+                Console.Write(num1 - num2);
+            }
+             else if (op == "/")
+            {
+                Console.Write(num1 / num2);
+            }
+             else if (op == "*")
+            {
+                Console.Write(num1 * num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
+
+    Console.ReadLine();
+        }
+    }
+}
